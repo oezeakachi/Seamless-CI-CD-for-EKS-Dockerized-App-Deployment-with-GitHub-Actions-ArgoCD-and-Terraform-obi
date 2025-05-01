721ae188e0df5a1027dcadd883f308de44c0f5c9
oezeakachi <oezeakac@redhat.com>
2025-05-01 18:34:17 +0100

    Adding service

--- completed-project/infrastructure/terraform-manifest/4_argocd.tf
+++ completed-project/infrastructure/terraform-manifest/4_argocd.tf
@@ -1,13 +1,20 @@
 # 4_argoCD.tf
 resource "helm_release" "argocd" {
-  name = "argocd"
+  name             = "argocd"
   repository       = "https://argoproj.github.io/argo-helm"
   chart            = "argo-cd"
   namespace        = "argocd"
   version          = "4.9.7"
   create_namespace = true
 
+  values = [
+    # Define the value to expose the server service as LoadBalancer
+    <<-EOF
+    server:
+      service:
+        type: LoadBalancer
+    EOF
+  ]
 
   depends_on = [module.eks-cluster]
-
 }
